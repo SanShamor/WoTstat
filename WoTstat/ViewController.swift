fce0c3f415efec9da4f6f958e31e6fa2468459dc
SanShamor <sanshamor@mail.ru>
2021-12-09 15:46:58 +0300

    Initial Commit

---
+++ WoTstat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WoTstat
+//
+//  Created by Александр on 09.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
